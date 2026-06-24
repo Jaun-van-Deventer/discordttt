1b77485a2f1e34e2183fb35ca4fbaa2671b2be1d
Jaun van Deventer <112894912+Jaun-van-Deventer@users.noreply.github.com>
2026-06-24 18:25:55 +0200

    refactor: added allow all to cors origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ['GET', 'POST'],
   },
 })
