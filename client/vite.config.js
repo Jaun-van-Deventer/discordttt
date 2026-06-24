08e6a116b2850314db7fc41deec26c9fe84c344c
Jaun van Deventer <112894912+Jaun-van-Deventer@users.noreply.github.com>
2026-06-24 20:21:53 +0200

    fix: fixed env variable usage

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,27 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: '../',
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: import.meta.env.VITE_SERVER_URL || 'http://localhost:3001',
+      '/.proxy/api': {
+        target: process.env.VITE_SERVER_URL || 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/.proxy\/api/, ''),
+      },
+      '/.proxy/server': {
+        target: process.env.VITE_SERVER_URL || 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/.proxy\/server/, ''),
         ws: true,
       },
     },
-    '/server': import.meta.env.VITE_SERVER_URL || 'http://localhost:3001',
     hmr: {
       clientPort: 443,
     },
   },
-});
+})
